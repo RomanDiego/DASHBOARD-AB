ac03be3fd8b977a5043ae7fe0e70a8a2d230f8d2
RomanDiego <87847951+RomanDiego@users.noreply.github.com>
2026-06-02 15:47:47 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1243,7 +1243,7 @@ elif menu == "Rotación de Productos":
 
     df_rot = df.copy()
 
-if clasificacion != "TODAS":
+    if clasificacion != "TODAS":
         df_rot = df_rot[
             df_rot["CLASIFICACION"] == clasificacion
         ]
@@ -1491,17 +1491,17 @@ else:
         st.subheader(
             "🚦 Estado de Rotación"
         )
-if promedio_mensual >= 100:
+    if promedio_mensual >= 100:
             st.success(
                 "🟢 Alta Rotación"
             )
 
-elif promedio_mensual >= 20:
+    elif promedio_mensual >= 20:
             st.warning(
                 "🟡 Rotación Media"
             )
 
-else:
+    else:
             st.error(
                 "🔴 Baja Rotación"
             )
