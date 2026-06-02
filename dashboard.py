cbbb50b5ffd46f222bd2c2084c4936020c855bcd
RomanDiego <87847951+RomanDiego@users.noreply.github.com>
2026-06-02 15:50:53 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1491,17 +1491,17 @@ else:
         st.subheader(
             "🚦 Estado de Rotación"
         )
-    if promedio_mensual >= 100:
+if promedio_mensual >= 100:
             st.success(
                 "🟢 Alta Rotación"
             )
 
-    elif promedio_mensual >= 20:
+elif promedio_mensual >= 20:
             st.warning(
                 "🟡 Rotación Media"
             )
 
-    else:
+else:
             st.error(
                 "🔴 Baja Rotación"
             )
